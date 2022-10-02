f4725e21fe6962cc22dc7fe8e5f87b2aee96681f
Ann Schnabel <anntschnabel@gmail.com>
2022-10-02 14:13:27 -0400

    Initial setup

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+const { ethers } = require("hardhat");
+
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomicfoundation/hardhat-chai-matchers");
+require("hardhat-docgen");
+require("solidity-coverage");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+};
